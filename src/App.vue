cf8c710ba7901e65acab2ed94a52e9e890d34c55
franciowel <spina_francesco@blu.it>
2022-07-05 15:24:10 +0200

    modificato el HelloWorld con myMessage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <myMessage msg="La mia prima app con Vue CLI, modifica h1"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myMessage from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myMessage
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'myMessage',
   props: {
     msg: String
   }
